0089dcbd8eca5249156e528740a3397c20f5cbf6
Ferd1xx <zin.andr2004@gmail.com>
2025-11-24 17:56:04 +0200

    hm 3

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,36 +2,8 @@ import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res) => {
-  const page = Number(req.query.page) || 1;
-  const perPage = Number(req.query.perPage) || 10;
-  const { tag, search } = req.query;
-
-  const skip = (page - 1) * perPage;
-  const filter = {};
-
-  if (tag) {
-    filter.tag = tag;
-  }
-  if (search) {
-    filter.$text = { $search: search };
-  }
-
-  let notesQuery = Note.find(filter);
-
-  const [totalNotes, notes] = await Promise.all([
-    notesQuery.clone().countDocuments(),
-    notesQuery.skip(skip).limit(perPage),
-  ]);
-
-  const totalPages = Math.ceil(totalNotes / perPage);
-
-  res.status(200).json({
-    page,
-    perPage,
-    totalNotes,
-    totalPages,
-    notes,
-  });
+  const notes = await Note.find();
+  res.status(200).json(notes);
 };
 
 export const getNoteById = async (req, res, next) => {
